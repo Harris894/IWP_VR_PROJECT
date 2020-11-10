d75185af73c931da1a391f7fb9e6909ce990de98
KrasiStoyanov <krasimir641@gmail.com>
2020-11-10 11:26:53 +0100

    Added NavMesh and basic object movement controller.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
