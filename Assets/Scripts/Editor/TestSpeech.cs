ca8056b6ff0eb182e986c002d94150983c62fb8b
Harris <x.arisakritidis@hotmail.com>
2020-11-24 13:39:27 +0100

    Included an easy testButton for speech.
    Go to speechmanager>click button in inspector called : StartListening()

---
+++ Assets/Scripts/Editor/TestSpeech.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpeechRecognition))]
+public class TestSpeech : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SpeechRecognition listeningEvent = (SpeechRecognition)target;
+
+        if (GUILayout.Button("StartListening()"))
+        {
+            listeningEvent.StartContinuous();
+        }
+    }
+}
