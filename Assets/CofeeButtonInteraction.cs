c0c7bfc1da624a248e43e692fc1e93d3b303163a
Harris <x.arisakritidis@hotmail.com>
2020-12-08 12:17:59 +0100

    Added 2 ugly buttons on the coffeemachine

---
+++ Assets/CofeeButtonInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CofeeButtonInteraction : MonoBehaviour
+{
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("index"))
+        {
+            anim.SetTrigger("ButtonPressed");
+            //call function to start pouring coffee
+        }
+    }
+
+}
